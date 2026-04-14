babe1f1dc14bf5634fc620b05ece842bfde07f31
Nickah-crea <andrianarisoa3544@gmail.com>
2026-04-14 10:11:40 +0200

    Premier commit

---
+++ backend-vdfi-mpihira/src/main/java/com/example/backend_vdfi_mpihira/BackendVdfiMpihiraApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_vdfi_mpihira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendVdfiMpihiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendVdfiMpihiraApplication.class, args);
+	}
+
+}
---
+++ backend-vdfi-mpihira/src/test/java/com/example/backend_vdfi_mpihira/BackendVdfiMpihiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_vdfi_mpihira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendVdfiMpihiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

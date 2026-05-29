47adb099956a512888cbc7324e589a0fc44db2d0
Nickah-crea <andrianarisoa3544@gmail.com>
2026-05-29 12:01:14 +0200

    main.js

--- frontend-vdfi-mpihira/src/main.js
+++ frontend-vdfi-mpihira/src/main.js
@@ -2,6 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/css/style.css'
+import './assets/css/base/variables.css'
+
+const cssModules = import.meta.glob('./assets/css/**/*.css', { eager: true, import: 'default' })
+Object.values(cssModules).forEach(css => {
+  // Le CSS est déjà injecté par Vite
+})
 
 createApp(App)
   .use(router)
